770dfeed04cf6e9c8b7ed34447f4b75b6c91c519
James Cho <hwisoocho@gmail.com>
2020-04-16 18:48:54 -0500

    route parameters in express

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -3,5 +3,5 @@ import express from 'express';
 const app = express();
 
 app.get('/hello', (req, res) => res.send('Hello!'));
-
+app.get('/hello/:name', (req, res) => res.send(`Hello ${req.params.name}`));
 app.listen(8000, ()=> console.log('Listening on port 8000'));
