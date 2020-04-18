e99170d97b406694e0488fc143de3d9ba411a9b9
James Cho <hwisoocho@gmail.com>
2020-04-17 19:50:05 -0500

    upvoting articles

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -1,7 +1,26 @@
+import bodyParser from 'body-parser';
+
+const articlesInfo = {
+  'learn-react': {
+    upvotes: 0,
+  },
+  'learn-node': {
+    upvotes: 0,
+  },
+  'learn-angular': {
+    upvotes: 0,
+  },
+}
+
 import express from 'express';
 
 const app = express();
+app.use(bodyParser.json());
 
-app.get('/hello', (req, res) => res.send('Hello!'));
-app.get('/hello/:name', (req, res) => res.send(`Hello ${req.params.name}`));
+app.post('/api/articles/:name/upvote', (req, res) => {
+  const articleName = req.params.name;
+
+  articlesInfo[articleName].upvotes += 1;
+  res.status(200).send(`${articleName} now has ${articleInfo[articleName].upvotes}`)
+})
 app.listen(8000, ()=> console.log('Listening on port 8000'));
